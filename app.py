351ce4e927d1d4be014ba00c5ef6b3798dbca207
gelwuyl <163805686+gelwuyl@users.noreply.github.com>
2024-03-18 17:02:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ model = {
 os.environ["REPLICATE_API_TOKEN"]="r8_GK2JD0n2YkmAffAeWciRy2ZHTh9f8ww1X5bTE"
 
 
-app = Flask(__name__)
-
 @app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
@@ -27,17 +25,16 @@ def palm_flask():
 def midjourney():
     return(render_template("midjourney.html"))
 
-
 @app.route("/palm_query", methods=["GET","POST"])
 def palm_query():
     q = request.form.get("q")
+    print(q)
     r = palm.chat(
         **model,
         messages=q
     )
     print(r.last)
     return(render_template("palm_reply.html",r=r.last))
-
 
 @app.route("/midjourney_query", methods=["GET","POST"])
 def midjourney_query():
@@ -46,7 +43,6 @@ def midjourney_query():
         "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", 
         input={"prompt": q}
     )
-    
     return(render_template("midjourney_reply.html",r=r[0]))
 
 if __name__ == "__main__":
